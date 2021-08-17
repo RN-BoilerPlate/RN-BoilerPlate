75c3b96d4f79b0dbe5e959621f89762119077877
ww8007 <wshmin@naver.com>
2021-08-18 01:14:26 +0900

    Feat : add Home page Just Test for render well

---
+++ RN_BoilerPlate/src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>hi</Text>
+    </View>
+  );
+};
+
+export default Home;

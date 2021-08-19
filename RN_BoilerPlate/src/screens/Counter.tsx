89199b5259b7f3b10eeb8b4c80004f602711a796
ww8007 <wshmin@naver.com>
2021-08-20 01:12:13 +0900

    Design : add color in botton

--- RN_BoilerPlate/src/screens/Counter.tsx
+++ RN_BoilerPlate/src/screens/Counter.tsx
@@ -26,13 +26,22 @@ const Counter: React.FC = () => {
     <View style={styles.view}>
       <Text style={styles.boldText}>Counter: {number}</Text>
       <View style={styles.buttonView}>
-        <Button mode="contained" style={styles.button} onPress={onIncrease}>
+        <Button
+          mode="contained"
+          style={[styles.button, {backgroundColor: 'red'}]}
+          onPress={onIncrease}>
           Increase
         </Button>
-        <Button mode="contained" style={styles.button} onPress={onReset}>
+        <Button
+          mode="contained"
+          style={[styles.button, {backgroundColor: 'green'}]}
+          onPress={onReset}>
           Reset
         </Button>
-        <Button mode="contained" style={styles.button} onPress={onDecrease}>
+        <Button
+          mode="contained"
+          style={[styles.button, {backgroundColor: 'blue'}]}
+          onPress={onDecrease}>
           Decrease
         </Button>
       </View>
@@ -41,11 +50,16 @@ const Counter: React.FC = () => {
 };
 
 export const styles = StyleSheet.create({
-  contributor: {marginBottom: '2%'},
   view: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  buttonView: {display: 'flex', flexDirection: 'column'},
-  boldText: {fontWeight: 'bold'},
-  button: {flexShrink: 1, width: '80vw'},
+  buttonView: {flex: 1, flexDirection: 'column'},
+  boldText: {
+    flex: 1,
+    fontSize: 25,
+    color: 'black',
+    fontWeight: 'bold',
+    marginTop: '30%',
+  },
+  button: {width: 200, marginTop: 20, color: 'white'},
 });
 
 export default Counter;

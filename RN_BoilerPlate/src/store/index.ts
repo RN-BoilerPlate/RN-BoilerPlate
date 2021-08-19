37aa2902fc4100ce20a760f29d3b53920dc9e1a0
ww8007 <wshmin@naver.com>
2021-08-20 01:13:46 +0900

    Feat : add sample, loading reducer add sampleSaga → rootSaga

--- RN_BoilerPlate/src/store/index.ts
+++ RN_BoilerPlate/src/store/index.ts
@@ -3,16 +3,18 @@ import {all} from 'redux-saga/effects';
 import {persistReducer} from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import counter from './counter';
-import storageSession from 'redux-persist/lib/storage/session';
+import sample, {sampleSaga} from './sample';
+import loading from './loading';
+// import storageSession from 'redux-persist/lib/storage/session';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
   whitelist: [''],
 };
-const rootReducer = combineReducers({counter});
+const rootReducer = combineReducers({counter, sample, loading});
 export default persistReducer(persistConfig, rootReducer);
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([]);
+  yield all([sampleSaga()]);
 }
